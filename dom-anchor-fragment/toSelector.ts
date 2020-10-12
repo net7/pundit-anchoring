6b11d2ab4e031436d622fa65ad94d68c9767d52d
Edgar Gomez <gomez@netseven.it>
2020-10-12 16:49:55 +0200

    removed unused type

--- dom-anchor-fragment/toSelector.ts
+++ dom-anchor-fragment/toSelector.ts
@@ -1,16 +1,14 @@
-type ToSelectorResult = {
-  type: 'FragmentSelector',
-  value: string,
-  conformsTo: string;
-};
-
 const HTML_CONFORMS_TO_TEXT = 'https://tools.ietf.org/html/rfc3236';
 const SVG_CONFORMS_TO_TEXT = 'http://www.w3.org/TR/SVG/';
 
 const toSelector = (
   root: HTMLElement | SVGSVGElement,
   id: string
-): ToSelectorResult => {
+): {
+  type: 'FragmentSelector',
+  value: string,
+  conformsTo: string;
+} => {
   const el = root.querySelector(`#${id}`);
   if (el === null) {
     throw new Error(`no element found with id "${id}"`);

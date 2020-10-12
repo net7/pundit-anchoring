4ee427389c642168c7e0134221de5b63172d5d2e
Edgar Gomez <gomez@netseven.it>
2020-10-12 16:23:46 +0200

    added fromSelector

--- dom-anchor-fragment/fromSelector.ts
+++ dom-anchor-fragment/fromSelector.ts
@@ -1,1 +1,9 @@
-// TODO
+const fromSelector = (
+  root: HTMLElement,
+  selector: { value: string }
+): {
+  root: HTMLElement;
+  id: string | null;
+} => ({ root, id: selector.value });
+
+export default fromSelector;
--- tests/dom-anchor-fragment/fromSelector.spec.ts
+++ tests/dom-anchor-fragment/fromSelector.spec.ts
@@ -1,1 +1,25 @@
-// TODO
+import { expect } from 'chai';
+import 'mocha';
+import { JSDOM } from 'jsdom';
+import htmlMock from './mocks/html';
+import fromSelector from '../../dom-anchor-fragment/fromSelector';
+
+describe('DOM anchor fragment - fromSelector', () => {
+  let dom: JSDOM;
+  let root: HTMLElement;
+
+  beforeEach(() => {
+    dom = new JSDOM(htmlMock);
+
+    const { document } = dom.window;
+    root = document.getElementById('root');
+  });
+
+  it('returns an id equals to the value of the selector', () => {
+    const selector = {
+      value: 'test-id',
+    };
+    const result = fromSelector(root, selector);
+    expect(result.id).eq(selector.value);
+  });
+});

104d9689517e06b32431b8f0b1175683e70a47fd
Edgar Gomez <gomez@netseven.it>
2020-10-12 16:24:03 +0200

    added toRange

--- dom-anchor-fragment/toRange.ts
+++ dom-anchor-fragment/toRange.ts
@@ -1,1 +1,16 @@
-// TODO
+const toRange = (
+  root: HTMLElement,
+  id: string
+): Range => {
+  const el = root.querySelector(`#${id}`);
+  if (el === null) {
+    throw new Error(`no element found with id "${id}"`);
+  }
+
+  const range = root.ownerDocument.createRange();
+  range.selectNodeContents(el);
+
+  return range;
+};
+
+export default toRange;
--- tests/dom-anchor-fragment/toRange.spec.ts
+++ tests/dom-anchor-fragment/toRange.spec.ts
@@ -1,1 +1,30 @@
-// TODO
+import { expect } from 'chai';
+import 'mocha';
+import { JSDOM } from 'jsdom';
+import htmlMock from './mocks/html';
+import toRange from '../../dom-anchor-fragment/toRange';
+
+describe('DOM anchor fragment - toRange', () => {
+  let dom: JSDOM;
+  let body: HTMLElement;
+  let root: HTMLElement;
+
+  beforeEach(() => {
+    dom = new JSDOM(htmlMock);
+
+    const { document } = dom.window;
+    body = document.body;
+    root = document.getElementById('root');
+  });
+
+  it('throws an error if no element exists with the stored id', () => {
+    expect(() => {
+      toRange(root, 'bogus');
+    }).to.throw('no element found');
+  });
+
+  it('returns a range selecting the contents of the element', () => {
+    const range = toRange(body, root.id);
+    expect(range.commonAncestorContainer).eq(root);
+  });
+});

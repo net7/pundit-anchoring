08482544fa1ad3ce0f5b35399f6ca0275ffb2bab
Edgar Gomez <gomez@netseven.it>
2020-10-12 16:47:31 +0200

    added toSelector

--- dom-anchor-fragment/toSelector.ts
+++ dom-anchor-fragment/toSelector.ts
@@ -1,1 +1,28 @@
-// TODO
+type ToSelectorResult = {
+  type: 'FragmentSelector',
+  value: string,
+  conformsTo: string;
+};
+
+const HTML_CONFORMS_TO_TEXT = 'https://tools.ietf.org/html/rfc3236';
+const SVG_CONFORMS_TO_TEXT = 'http://www.w3.org/TR/SVG/';
+
+const toSelector = (
+  root: HTMLElement | SVGSVGElement,
+  id: string
+): ToSelectorResult => {
+  const el = root.querySelector(`#${id}`);
+  if (el === null) {
+    throw new Error(`no element found with id "${id}"`);
+  }
+
+  return {
+    type: 'FragmentSelector',
+    value: id,
+    conformsTo: el instanceof SVGElement
+      ? SVG_CONFORMS_TO_TEXT
+      : HTML_CONFORMS_TO_TEXT,
+  };
+};
+
+export default toSelector;
--- tests/dom-anchor-fragment/toSelector.spec.ts
+++ tests/dom-anchor-fragment/toSelector.spec.ts
@@ -1,1 +1,55 @@
-// TODO
+import { expect } from 'chai';
+import 'mocha';
+import { JSDOM } from 'jsdom';
+import htmlMock from './mocks/html';
+import toSelector from '../../dom-anchor-fragment/toSelector';
+
+describe('DOM anchor fragment - toSelector', () => {
+  let dom: JSDOM;
+  let body: HTMLElement;
+  let root: HTMLElement;
+
+  beforeEach(() => {
+    dom = new JSDOM(htmlMock);
+
+    // globals
+    global.SVGElement = dom.window.SVGElement;
+    global.document = dom.window.document;
+
+    body = document.body;
+    root = document.getElementById('root');
+  });
+
+  it('returns a selector from an HTMLElement', () => {
+    const selector = toSelector(body, root.id);
+    expect(selector).to.deep.equal({
+      type: 'FragmentSelector',
+      value: root.id,
+      conformsTo: 'https://tools.ietf.org/html/rfc3236'
+    });
+  });
+
+  it('returns a selector from an SVGElement', () => {
+    const svg = document.createElementNS(
+      'http://www.w3.org/2000/svg', 'svg'
+    );
+    const rect = document.createElementNS(
+      'http://www.w3.org/2000/svg', 'rect'
+    );
+    rect.id = 'rectangle1';
+    root.appendChild(svg);
+    svg.appendChild(rect);
+    const selector = toSelector(svg, rect.id);
+    expect(selector).to.deep.equal({
+      type: 'FragmentSelector',
+      value: rect.id,
+      conformsTo: 'http://www.w3.org/TR/SVG/'
+    });
+  });
+
+  it('throws an error if no element exists with the stored id', () => {
+    expect(() => {
+      toSelector(root, 'bogus');
+    }).to.throw('no element found');
+  });
+});
